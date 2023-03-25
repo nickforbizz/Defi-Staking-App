e1df854214a9e0f51271f24edd6c38a49a3241a7
Nicholas Waruingi <NWaruingi@kemri-wellcome.org>
2023-03-25 14:06:21 +0300

    contracts tokens

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,8 +3,28 @@ const RWD = artifacts.require('RWD');
 const DecentralBank = artifacts.require('DecentralBank');
 
 
-module.exports = async function (deployer) {
+module.exports = async function (deployer, network, accounts) {
+    // Deploy Mock Tether Contract
     await deployer.deploy(Tether);
+    const tether = await Tether.deployed();
+
+
+    // Deploy RWD Contract
     await deployer.deploy(RWD);
-    await deployer.deploy(DecentralBank);
+    const rwd = await RWD.deployed();
+
+
+    // Deploy DecentralBank Contract
+    await deployer.deploy(DecentralBank, rwd.address, tether.address);
+    const decentralBank = await DecentralBank.deployed();
+
+
+
+    // Transfer RWD tokens to DecentralBank
+    await rwd.transfer(decentralBank.address, '1000000000000000000000000');
+
+
+
+    // Distribute 100 tokens to investors
+    await tether.transfer(accounts[1], '100000000000000000000');
 }
--- src/contracts/DecentralBank.sol
+++ src/contracts/DecentralBank.sol
@@ -1,9 +1,21 @@
 pragma solidity ^0.8.19;
 
 
+import './Tether.sol';
+import './RWD.sol';
+
 contract DecentralBank{
     address public owner;
-    string public name = "Decentral Bank";   
+    string public name = "Decentral Bank";  
+
+    Tether public tether; 
+    RWD public rwd; 
+
+
+    constructor(RWD _rwd, Tether _tether){
+        rwd = _rwd;
+        tether = _tether;
+    }
 
 
 }
